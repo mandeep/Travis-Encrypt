df8ea13095fe9707f19be933a8f745b29758519e
mandeepbhutani <bhutanimandeep@gmail.com>
2016-09-11 19:44:04 -0500

    Added tests

--- travis/tests/test_encrypt.py
+++ travis/tests/test_encrypt.py
@@ -1,1 +1,24 @@
-from travis import encrypt
+from click.testing import CliRunner
+import pytest
+from travis.encrypt import cli, encrypt_key, retrieve_public_key
+
+
+@pytest.fixture
+def repository():
+    return 'mandeep/Mosaic'
+
+
+def test_public_key_retrieval(repository):
+    """Tests encrypt's retrieve_public_key function."""
+    public_key = retrieve_public_key(repository)
+    assert isinstance(public_key, str)
+    assert 'BEGIN PUBLIC KEY' in public_key
+    assert 'END PUBLIC KEY' in public_key
+
+
+def test_encrypt_key(repository):
+    """Tests encrypt's encrypt_key function."""
+    public_key = retrieve_public_key(repository)
+    password = 'SUPER_SECURE_PASSWORD'
+    encrypted_password = encrypt_key(public_key, password.encode())
+    assert isinstance(encrypted_password, bytes)
