057f0733fa58d1a087a31a79194457e0cf60c596
mandeepbhutani <bhutanimandeep@gmail.com>
2016-09-12 21:23:49 -0500

    Fixed issue with yaml loading

--- travis/tests/test_encrypt.py
+++ travis/tests/test_encrypt.py
@@ -1,6 +1,7 @@
 from click.testing import CliRunner
 import pytest
 from travis.encrypt import cli, encrypt_key, retrieve_public_key
+import yaml
 
 
 @pytest.fixture
@@ -29,9 +30,10 @@ def test_cli():
     """Tests encrypt's cli function."""
     runner = CliRunner()
     with runner.isolated_filesystem():
+        initial_data = {'language': 'python'}
         with open('file.yml', 'w') as file:
-            file.write('Test')
+            yaml.dump(initial_data, file, default_flow_style=True)
 
         result = runner.invoke(cli, ['mandeep', 'Travis-Encrypt', 'file.yml'],
                                'SUPER_SECURE_PASSWORD')
-        assert 'Password' in result.output
+        assert not result.exception
