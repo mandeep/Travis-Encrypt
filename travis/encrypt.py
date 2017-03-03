b2bd56b753dc3992109762071d4a535c686966b5
mandeep <mandeep@users.noreply.github.com>
2017-03-03 00:02:01 -0600

    Added a line break prior to printing password to terminal

--- travis/encrypt.py
+++ travis/encrypt.py
@@ -85,5 +85,5 @@ def cli(username, repository, path, password, deploy, env):
         print('Encrypted password added to {}' .format(path))
 
     else:
-        print('Please add the following to your .travis.yml:\n\nsecure: "{}"\n'
+        print('\nPlease add the following to your .travis.yml:\n\nsecure: "{}"\n'
               .format(encrypted_password))
