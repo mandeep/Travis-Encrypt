e7d8ae0a9bc66dc7166ae4f5f8f6d8c5dd948967
mandeep <mbhutani@continuum.io>
2017-05-22 16:41:35 -0500

    Removed support for Python 3.3

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setup(name='travis-encrypt',
       classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
       ],
       )
